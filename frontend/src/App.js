1b0a4a3cdef66f13eb0054d156e72bbdcb39ce52
kunal <kunal8817@github.com>
2022-12-10 10:54:03 +0530

    Controlled seach component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,23 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
-const handleSearchSubmit = (e) => {
-  e.preventDefault();
-  console.log(e.target[0].value);
-}
 
-function App() {
+const App = () => {
+  const [word, setWord] = useState('')
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
+
+  console.log(word);
+
   return (
-    <div className="App">
+    <div>
       <Header title="Images Gallery"/>
-      <Search handleSubmit={handleSearchSubmit}/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
