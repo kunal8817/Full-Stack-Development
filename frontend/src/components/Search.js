de6efdba0e23427cb44e41dfe329c767637316b5
kunal <kunal8817@github.com>
2022-12-10 10:24:13 +0530

    Updated the size

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -3,13 +3,13 @@ import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 const Search = () => {
     return (
-        <Container>
-            <Row>
-                <Col>
+        <Container className="mt-4">
+            <Row className="justify-content-center">
+                <Col xs={12} md={8}>
                     <Form>
                         <Form.Row>
                             <Col>
-                                <Form.Control placeholder="Search for the Image that you are looking for.." />
+                                <Form.Control placeholder="Search for the Image.." />
                             </Col>
                             <Col>
                                 <Button variant="primary" type="submit">Search</Button>
