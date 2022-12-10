2cf67d8fe3c482b66c1715927c7d0fb0f717e8cb
kunal <kunal8817@github.com>
2022-12-10 10:39:24 +0530

    Updated the search component

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-const Search = () => {
+const Search = (handleSubmit) => {
     return (
         <Container className="mt-4">
             <Row className="justify-content-center">
                 <Col xs={12} md={8}>
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <Form.Row>
                             <Col>
                                 <Form.Control placeholder="Search for the Image.." />
