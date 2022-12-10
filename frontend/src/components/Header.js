4ce477aa3e77e7acf2e33db3375d7457639ded5f
kunal <kunal8817@github.com>
2022-12-10 10:26:57 +0530

    Updated the container function in Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 
 const Header = ({ title }) => {
     return (
         <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="/">{title}</Navbar.Brand>
+            <Container>
+                <Navbar.Brand href="/">{title}</Navbar.Brand>
+            </Container>
         </Navbar>
     )
 }
